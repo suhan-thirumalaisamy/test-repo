111f41f06607c47885e493749f1b801212f74238
Suhan <suhan.thirumalai@rootquotient.com>
2026-05-14 09:18:14 +0530

    Update port in qa

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import usersRoutes from "./routes/users.routes";
 import productsRoutes from "./routes/products.routes";
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(express.json());
